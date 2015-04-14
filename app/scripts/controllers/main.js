21a0a901d26273a6a61265f3d0df869362dd7604
Arthur Head <arthurallenhead@yahoo.com>
2015-04-13 22:26:51 -0700

    initial commit - added images and headings with ng-repeat

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,45 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zyringApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zyringApp
+ */
+var zyringApp = angular.module('zyringApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.cities = [
+		{
+			city : "Seattle",
+			image : "http://goo.gl/mIffQj"
+		},
+		{
+			city : "Tacoma",
+			image : "http://goo.gl/YLtGMD"
+		},
+		{
+			city : "Redmond",
+			image : "http://goo.gl/wY72hU"
+		},
+		{
+			city : "Bellvue",
+			image : "http://goo.gl/ydvsWh"
+		},
+		{
+			city : "Kirkland",
+			image : "http://goo.gl/UrHgbk"
+		},
+		{
+			city : "Olympia",
+			image : "http://goo.gl/ngqBfj"
+		}
+	];
+  });
+
